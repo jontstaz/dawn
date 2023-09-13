e66b76eb00a7049da6e9989b9732220252522d0e
jontstaz <jonteoz@hotmail.com>
2023-09-13 12:00:50 +1000

    set the latitude and longitude manually

--- src/components/weather/weather.api.js
+++ src/components/weather/weather.api.js
@@ -1,7 +1,7 @@
 class WeatherForecastClient {
   constructor(location) {
     this.appId = '50a34e070dd5c09a99554b57ab7ea7e2';
-    this.url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(location)}&units=metric&appid=${this.appId}`;
+    this.url = `https://api.openweathermap.org/data/2.5/weather?lat=-37.814&lon=144.9633&units=metric&appid=${this.appId}`;
   }
 
   async getWeather() {
